05489639236852b3fcb8050df88488e54ae1cb3c
Chris <chris.anthony.yee@gmail.com>
2018-02-24 19:29:27 -0600

    added user retrieval

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,18 +1,19 @@
 require 'httparty'
+require 'json'
 
 class Kele
   include HTTParty
 
 
   def initialize(email, password)
-    @api_url = 'https://www.bloc.io/api/v1'
+    @api_uri = 'https://www.bloc.io/api/v1'
     options = {
       body: {
         email: email,
         password: password
       }
     }
-    response = self.class.post "#{@api_url}/sessions", options
+    response = self.class.post "#{@api_uri}/sessions", options
 
     if response.code == 200
       p "Welcome #{email}!"
@@ -21,5 +22,11 @@ class Kele
       p "Email or password are incorrect"
 
     end
+    @auth_token = response["auth_token"]
+  end
+
+  def get_me
+    response = self.class.get("#{@api_uri}/users/me", headers: { "authorization" => @auth_token })
+    @user_data = JSON.parse(response.body)
   end
 end
