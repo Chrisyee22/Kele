5b854c074942469676415788085b8a21ee2e0abe
Chris <chris.anthony.yee@gmail.com>
2018-03-01 12:12:14 -0800

    messages

--- lib/kele.rb
+++ lib/kele.rb
@@ -33,10 +33,30 @@ class Kele
   end
 
   def get_mentor_availability(mentor_id)
-    response = self.class.get("#{@api_uri}/mentors/#{mentor_id}/student_availability", headers: {
-      "authorization" => @auth_token})
+    response = self.class.get("#{@api_uri}/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token})
       @mentor_availability = JSON.parse(response.body)
   end
 
+  def get_messages(page = nil)
+    if page
+        response = self.class.get("#{@api_uri}/message_threads?page=#{page}", headers: {"authorization" => @auth_token})
+    else
+      response = self.class.get("#{@api_uri}/message_threads", headers: { "authorization" => @auth_token })
+    end
+      @messages = JSON.parse(response.body)
+  end
 
+  def create_message(sender_email, recipient_id, message_thread, subject, body)
+
+    response = self.class.post("#{@api_uri}/messages",
+    body: {
+      "sender": sender_email,
+      "recipient_id": recipient_id,
+      "token": message_thread,
+      "subject": subject,
+      "stripped-text": body
+    },
+    headers: {"authorization" => @auth_token})
+
+  end
 end
